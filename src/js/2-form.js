e0f0fed4841a040f5df336ee036692b93fd4b8c9
bondalina <142501520+bondalina@users.noreply.github.com>
2024-01-06 12:13:27 +0200

    check2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,22 +6,26 @@ const textarea = form.querySelector("textarea");
 form.addEventListener("submit", onFormSubmit);
 function onFormSubmit(event) {
     event.preventDefault();
+    if (emailInput.value.trim() === "" || textarea.value.trim() === "") {
+        alert("All form fields must be filled in");
+        return;
+    }
     localStorage.removeItem(STORAGE_KEY);
     event.currentTarget.reset();
 }
 
-emailInput.addEventListener("input", onInput);
-textarea.addEventListener("input", onInput);
+form.addEventListener("input", onInput);
+// textarea.addEventListener("input", onInput);
 function onInput(event) {
     const formInfo = {
-      email: emailInput.value,
-      message: textarea.value,
+      email: emailInput.value.trim(),
+      message: textarea.value.trim(),
     };
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formInfo));
   }
 
 function populateForm() {
-    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
     if (savedMessage.email) {
         emailInput.value = savedMessage.email;
     }
